d1f8f4eac3c57b09de19589b81da86b49925f90b
Anna <annakarpun@rambler.ru>
2023-06-25 16:55:06 +0300

    Семинар+ДЗ

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу,которая на вход принимает два числа 
+и проверяет,является ли первое число квадратом второго
+a=25; b=5-> да
+*/
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2*number2)
+Console.WriteLine ("true");
+else
+Console.WriteLine ("false");
+
---
+++ task04_трехзначное_число/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу,которая на вход принимает одно 
+трехзначное число и на выходе показывает последнюю
+цифру этого числа
+ 456- 6
+ 782 -2
+ */
+ Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int integer = 0;
+if (number < 100 || number > 999) Console.WriteLine("Введите трехзначное число");
+else 
+{ 
+    integer = number % 10;
+Console.WriteLine( integer );
+}
+
+
